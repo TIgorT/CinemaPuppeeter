7c9023ff3eee622d62c57cf9d32529c2b51c9ee2
Igor <TimoshenkoII@yandex.com>
2023-09-21 19:30:55 +0300

    Initial commit

---
+++ cinema.test.js
@@ -0,0 +1,29 @@
+const { clickElement, putText, getText } = require("./lib/commands.js");
+const { generateName } = require("./lib/util.js");
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Netology.ru tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://netology.ru");
+  });
+
+  test("The first test'", async () => {
+    const title = await page.title();
+    console.log("Page title: " + title);
+    await clickElement(page, "header a + a");
+    const title2 = await page.title();
+    console.log("Page title: " + title2);
+    const pageList = await browser.newPage();
+    await pageList.goto("https://netology.ru/navigation");
+    await pageList.waitForSelector("h1");
+  });
+});
